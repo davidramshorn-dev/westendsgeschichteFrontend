03d0cbb2e1860a0e7d587bdc49c044d30c9bbc97
David Ramshorn <david.ramshorn@gmx.de>
2026-06-05 19:22:06 +0200

    Fix: Pfade für Monorepo-Struktur final korert

--- interactiveMap/vite.config.js
+++ interactiveMap/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/interactiveMap/', // <-- Wichtig, damit die Skripte im Unterordner gesucht werden
+  base: './', // <-- Exakt so eintragen (Punkt und Schrägstrich)
 })
 
