ae8f0052eefa823c1b353e681d59bc32e1d5c4aa
David Ramshorn <david.ramshorn@gmx.de>
2026-06-05 17:42:10 +0200

    new Message

--- interactiveMap/src/main.jsx
+++ interactiveMap/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+alert("Hallo, JavaScript läuft!");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
