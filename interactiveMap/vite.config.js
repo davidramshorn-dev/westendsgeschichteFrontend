bd07721da5eec0c6c4b6d58c86a63549965fc6f2
David Ramshorn <david.ramshorn@gmx.de>
2026-06-05 19:09:33 +0200

    Fix: Kombiniertes Deployment für Root-HTML und Vite-Unterordner

--- interactiveMap/vite.config.js
+++ interactiveMap/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/interactiveMap/', // <-- Wichtig, damit die Skripte im Unterordner gesucht werden
 })
+
